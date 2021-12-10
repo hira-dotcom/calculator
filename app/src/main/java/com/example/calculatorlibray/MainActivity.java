7e3869e08e8815db5f0ade0b317786181cbe2f2a
Hira Akbar Ali <hiraremotescrach@gmail.com>
2021-12-10 12:38:31 +0500

    first commit

--- app/src/main/java/com/example/calculatorlibray/MainActivity.java
+++ app/src/main/java/com/example/calculatorlibray/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.calculatorlibray;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.calClass;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_sum ;
+    EditText ed1,ed2;
+    int v1,v2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_sum = findViewById(R.id.sum);
+        ed1 = findViewById(R.id.val1);
+        ed2 = findViewById(R.id.val2);
+        v1 = Integer.parseInt(ed1.getText().toString());
+        v2 = Integer.parseInt(ed2.getText().toString());
+        int result = calClass.add(MainActivity.this,v1,v2);
+
+        btn_sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, ""+result, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- calculator/src/main/java/com/calClass.java
+++ calculator/src/main/java/com/calClass.java
@@ -1,6 +1,12 @@
 package com;
 
+import android.content.Context;
+
 public class calClass {
 
+    public  static int add(Context c, int a, int b){
+       int result =a+b;
+        return result;
+    }
 
 }
