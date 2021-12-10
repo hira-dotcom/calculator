07acf4bd0e3b2734e77d787c23b0079fe55cad18
Hira Akbar Ali <hiraremotescrach@gmail.com>
2021-12-10 14:24:12 +0500

    first commit

--- calculator/src/main/java/com/example/AddActivity.java
+++ calculator/src/main/java/com/example/AddActivity.java
@@ -14,20 +14,21 @@ public class AddActivity extends AppCompatActivity {
 
     Button btn_sum ;
     EditText ed1,ed2;
-    int v1,v2;
+    Double v1,v2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_sum = findViewById(R.id.sum);
-        ed1 = findViewById(R.id.val1);
-        ed2 = findViewById(R.id.val2);
+
         btn_sum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                v1 = Integer.parseInt(ed1.getText().toString());
-                v2 = Integer.parseInt(ed2.getText().toString());
-                int result = v1+v2;
+                ed1 = findViewById(R.id.val1);
+                ed2 = findViewById(R.id.val2);
+                v1 = Double.parseDouble(ed1.getText().toString());
+                v2 = Double.parseDouble(ed2.getText().toString());
+                Double result = v1+v2;
                 Toast.makeText(AddActivity.this, "result "+result, Toast.LENGTH_SHORT).show();
             }
         });
